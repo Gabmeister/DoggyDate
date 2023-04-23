832427939827d6d0d80295a1e832360c8a42efcf
Adam O'Shea <C20372181@mytudublin.ie>
2023-04-23 15:39:31 +0200

    created image upload method

--- app/src/main/java/com/example/doggydateapp/MainActivity.java
+++ app/src/main/java/com/example/doggydateapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.doggydateapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -36,4 +37,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
