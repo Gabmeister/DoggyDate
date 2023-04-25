c0c9a35a648ec42c9260388f028a9cbd2d9ff884
mendebto <98464323+mendebto@users.noreply.github.com>
2023-04-25 11:40:37 +0100

    Navbar done

--- app/src/main/java/com/example/doggydateapp/MainActivity.java
+++ app/src/main/java/com/example/doggydateapp/MainActivity.java
@@ -7,7 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity {
 
     private Button likeButton;
@@ -35,6 +35,22 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // code here for dislike button click/swipe
             }
+        });
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setSelectedItemId(R.id.match);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+
+                case R.id.match:
+                    return true;
+                case R.id.chat:
+                    return true;
+                case R.id.account:
+                    startActivity(new Intent(getApplicationContext(), AccountSettingsActivity.class));
+                    return true;
+            }
+            return false;
         });
     }
 
