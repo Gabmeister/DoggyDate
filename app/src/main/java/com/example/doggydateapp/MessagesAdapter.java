7a19abb45feac15d7160eff3abacdb0ec945231b
mendebto <98464323+mendebto@users.noreply.github.com>
2023-04-27 11:02:13 +0100

    Adapter

---
+++ app/src/main/java/com/example/doggydateapp/MessagesAdapter.java
@@ -0,0 +1,25 @@
+package com.example.doggydateapp;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+class MessagesAdapter extends RecyclerView.Adapter<MessagesAdapter.MyViewHolder> {
+
+    @NonNull
+    @Override
+    public MessagesAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MessagesAdapter.MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
